a06add425715b0657b2fad2511d8925a29954d7e
dipak kashinath gaikwad <dipakgaikwadmg@gmail.com>
2025-06-05 18:59:09 +0530

    deploy project to production

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+db#qns$pgyu!huhpnp+uvktmk#9-7^u*%phsfsxncrjfahcm$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 
 # Application definition
